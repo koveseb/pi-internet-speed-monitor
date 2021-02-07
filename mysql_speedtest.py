1cb0e890718a3a01278afff49eaaedd7477194c0
sebus208 <kovewebdesign@gmail.com>
2021-02-07 02:33:20 +0100

    mysql configuration & test

--- mysql_speedtest.py
+++ mysql_speedtest.py
@@ -1,31 +1,30 @@
-import os
+import config
 import re
-import subprocess
 import time
+import subprocess
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host=config.host, database=config.database, user=config.user, password=config.password
+)
 
 response = (
-    subprocess.Popen("/usr/bin/speedtest", shell=True, stdout=subprocess.PIPE)
+    subprocess.Popen("/usr/local/bin/speedtest-cli --simple", shell=True, stdout=subprocess.PIPE)
     .stdout.read()
     .decode("utf-8")
 )
+ping = re.findall("Ping:\s(.*?)\s", response, re.MULTILINE)
+download = re.findall("Download:\s(.*?)\s", response, re.MULTILINE)
+upload = re.findall("Upload:\s(.*?)\s", response, re.MULTILINE)
 
-ping = re.search("Latency:\s+(.*?)\s", response, re.MULTILINE)
-download = re.search("Download:\s+(.*?)\s", response, re.MULTILINE)
-upload = re.search("Upload:\s+(.*?)\s", response, re.MULTILINE)
+ping = ping[0].replace(",", ".")
+download = download[0].replace(",", ".")
+upload = upload[0].replace(",", ".")
 
-ping = ping.group(1)
-download = download.group(1)
-upload = upload.group(1)
+mycursor = mydb.cursor()
 
-try:
-    f = open("/home/pi/speedtest/speedtest.csv", "a+")
-    if os.stat("/home/pi/speedtest/speedtest.csv").st_size == 0:
-        f.write("Date,Time,Ping (ms),Download (Mbps),Upload (Mbps)\r\n")
-except:
-    pass
-
-f.write(
-    "{},{},{},{},{}\r\n".format(
-        time.strftime("%m/%d/%y"), time.strftime("%H:%M"), ping, download, upload
-    )
-)
+sql = "INSERT INTO customers (ping, download, upload) VALUES (%s, %s, %s)"
+val = (float(ping), float(download), float(upload))
+mycursor.execute(sql, val)
+mydb.commit()
+print(mycursor.rowcount, "record inserted.")
